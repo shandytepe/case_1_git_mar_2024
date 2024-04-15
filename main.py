570fa28ef5780d2f51027928c7cc13ab56328025
shandytepe <shandy@pacmannai.com>
2024-04-15 20:57:22 +0700

    update python script

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ blog_posts = [{'Photos': 3, 'Likes': 21, 'Comments': 2},
               {'Comments': 4, 'Shares': 2}, 
               {'Photos': 8, 'Comments': 1, 'Shares': 1}, 
               {'Photos': 3, 'Likes': 19, 'Comments': 3}]
+
+# fitur baru
+
+total_likes = 0
+
+try:
+    for post in blog_posts:
+        if 'Likes' not in post.keys():
+            post['Likes'] = 0
+            total_likes = total_likes + post['Likes']
+
+except:
+    pass
+
+print(total_likes)
